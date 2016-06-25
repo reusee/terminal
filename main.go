50649336135936c6945d8fae4bdfb517a9228392
reus <reusee@gmail.com>
2016-06-25 15:00:35 +0800

    update

--- main.go
+++ main.go
@@ -21,30 +21,30 @@ Gdk = lgi.Gdk
 Pango = lgi.Pango
 Vte = lgi.Vte
 
+local css_provider = Gtk.CssProvider()
+Gtk.StyleContext.add_provider_for_screen(
+	Gdk.Screen.get_default(),
+	css_provider,
+	Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+css_provider:load_from_data([[
+VteTerminal {
+	background-color: red;
+}
+]])
+
 local window = Gtk.Window{type = Gtk.WindowType.TOPLEVEL}
 window.on_destroy:connect(function()
 	Sys_exit()
 end)
 
-local css_provider = Gtk.CssProvider()
-css_provider:load_from_data([[
-	VteTerminal {
-		cursor-aspect-ratio: 0.8;
-	}
-]])
-Gtk.StyleContext.add_provider_for_screen(
-	Gdk.Screen.get_default(),
-	css_provider,
-	Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-
 local term = Vte.Terminal.new()
---term:set_cursor_shape(Vte.CursorShape.IBEAM)
+term:set_cursor_shape(Vte.CursorShape.IBEAM)
 term:set_cursor_blink_mode(Vte.CursorBlinkMode.OFF)
-term:set_font(Pango.FontDescription.from_string('Terminus 13'))
+term:set_font(Pango.FontDescription.from_string('Terminus 14'))
 term:set_color_cursor(Gdk.RGBA().parse('#fcaf17'))
 term:set_color_cursor_foreground(Gdk.RGBA().parse('black'))
 term:set_scrollback_lines(-1)
-term:set_scroll_on_output(true)
+--term:set_scroll_on_output(true)
 term:set_scroll_on_keystroke(true)
 term:set_rewrap_on_resize(true)
 term:set_encoding('UTF-8')
@@ -57,6 +57,14 @@ term:spawn_sync(
 	0,
 	function() end,
 	nil)
+term.on_child_exited = function()
+	Sys_exit()
+end
+term.on_button_press_event = function(widget, ev)
+	if ev.button == 3 then
+		term:copy_clipboard()
+	end
+end
 window:add(term)
 
 window:show_all()
