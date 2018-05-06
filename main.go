e222f0d371bfbdcbf4413810d90ec86df85a7c1d
reus <reusee@gmail.com>
2018-05-06 19:45:56 +0800

    style

--- main.go
+++ main.go
@@ -22,21 +22,26 @@ Gdk = lgi.Gdk
 Pango = lgi.Pango
 Vte = lgi.Vte
 
+local css_provider = Gtk.CssProvider()
+css_provider:load_from_data([[
+vte-terminal {
+    padding: 10px 20px;
+}
+]])
+Gtk.StyleContext.add_provider_for_screen(
+	Gdk.Screen.get_default(),
+	css_provider,
+	Gtk.STYLE_PROVIDER_PRIORITY_USER)
+
 local window = Gtk.Window{type = Gtk.WindowType.TOPLEVEL}
 window.on_delete_event = function()
 	return true
 end
 
 local term = Vte.Terminal.new()
-<<<<<<< HEAD
 term:set_cursor_shape(Vte.CursorShape.BLOCK)
 term:set_cursor_blink_mode(Vte.CursorBlinkMode.OFF)
 term:set_font(Pango.FontDescription.from_string('xos4 Terminus 12'))
-=======
---term:set_cursor_shape(Vte.CursorShape.IBEAM)
-term:set_cursor_blink_mode(Vte.CursorBlinkMode.OFF)
-term:set_font(Pango.FontDescription.from_string('xos4 Terminus 13'))
->>>>>>> 88cdc8b8a7bc43b38c9923690a6b93cd78191372
 term:set_color_cursor(Gdk.RGBA().parse('#fcaf17'))
 term:set_color_cursor_foreground(Gdk.RGBA().parse('black'))
 term:set_scrollback_lines(-1)
@@ -48,6 +53,23 @@ term:set_allow_bold(true)
 term:set_allow_hyperlink(true)
 term:set_mouse_autohide(true)
 term:set_cjk_ambiguous_width(2)
+term:set_colors(
+	nil,
+	nil,
+	{
+
+		-- solarized dark
+		--Gdk.RGBA().parse('#002b36'),
+		--Gdk.RGBA().parse('#073642'),
+		--Gdk.RGBA().parse('#586e75'),
+		--Gdk.RGBA().parse('#657b83'),
+		--Gdk.RGBA().parse('#839496'),
+		--Gdk.RGBA().parse('#93a1a1'),
+		--Gdk.RGBA().parse('#eee8d5'),
+		--Gdk.RGBA().parse('#fdf6e3')
+
+	}
+)
 
 term:spawn_sync(
 	Vte.PtyFlags.DEFAULT,
